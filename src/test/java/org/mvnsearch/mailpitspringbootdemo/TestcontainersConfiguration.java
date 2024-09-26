d17378ad5313d73879f8c4bc17696b98e569ca19
Libing Chen <libing.chen@gmail.com>
2024-09-26 13:22:52 +0800

    test: add MailpitClient bean

--- src/test/java/org/mvnsearch/mailpitspringbootdemo/TestcontainersConfiguration.java
+++ src/test/java/org/mvnsearch/mailpitspringbootdemo/TestcontainersConfiguration.java
@@ -1,18 +1,36 @@
 package org.mvnsearch.mailpitspringbootdemo;
 
+import org.mvnsearch.mailpitspringbootdemo.mail.MailpitClient;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import org.testcontainers.containers.GenericContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 class TestcontainersConfiguration {
     @Bean
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     public GenericContainer<?> mailpit(DynamicPropertyRegistry properties) {
         GenericContainer<?> mailpit = new GenericContainer<>("axllent/mailpit:v1.20.4").withExposedPorts(1025, 8025);
         properties.add("spring.mail.host", mailpit::getHost);
         properties.add("spring.mail.port", () -> mailpit.getMappedPort(1025).toString());
         return mailpit;
+    }
+
+    @Bean
+    @DependsOn("mailpit")
+    public MailpitClient mailpitClient(Environment environment) {
+        String host = environment.getProperty("spring.mail.host");
+        String port = environment.getProperty("spring.mail.port");
+        String httpBaseUrl = "http://" + host + ":" + port;
+        RestClient restClient = RestClient.builder().baseUrl(httpBaseUrl).build();
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+        return factory.createClient(MailpitClient.class);
     }
 }
