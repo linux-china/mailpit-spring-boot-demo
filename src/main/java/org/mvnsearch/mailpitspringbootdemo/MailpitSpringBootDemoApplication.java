7190bb284571d80d5d824d809c7845ce49ca32ea
Libing Chen <libing.chen@gmail.com>
2024-09-26 12:53:13 +0800

    app: MailpitSpringBootDemoApplication

---
+++ src/main/java/org/mvnsearch/mailpitspringbootdemo/MailpitSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.mailpitspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailpitSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailpitSpringBootDemoApplication.class, args);
+    }
+
+}
